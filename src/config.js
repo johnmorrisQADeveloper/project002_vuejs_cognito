d42e322f8573ff2344c82a1eeacfececece1da88
john morris <fennymorris@live.co.uk>
2020-05-11 19:01:13 +0100

    add gitnore for dist

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  getAllPost: 'https://9sewbzzxtg.execute-api.eu-west-2.amazonaws.com/Prod/posts',
-  updatePost: 'https://9sewbzzxtg.execute-api.eu-west-2.amazonaws.com/Prod/post',
-  createPost: 'https://9sewbzzxtg.execute-api.eu-west-2.amazonaws.com/Prod/post',
-  deletePost: 'https://9sewbzzxtg.execute-api.eu-west-2.amazonaws.com/Prod/post',
+  getAllPost: 'https://f9z2kblnf3.execute-api.eu-west-2.amazonaws.com/Stage/posts',
+  updatePost: 'https://f9z2kblnf3.execute-api.eu-west-2.amazonaws.com/Stage/post',
+  createPost: 'https://f9z2kblnf3.execute-api.eu-west-2.amazonaws.com/Stage/post',
+  deletePost: 'https://f9z2kblnf3.execute-api.eu-west-2.amazonaws.com/Stage/post',
   REGION: 'eu-west-2',
-  USER_POOL_ID: 'eu-west-2_TPMCbOl1I',
-  APP_CLIENT_ID: '79rbqj3e75cimfv8t3i5i1vv5l',
-  IDENTITY_POOL_ID: 'eu-west-2:29c673ed-5f7b-4f6a-b736-77e3f0d2bdb9'
+  USER_POOL_ID: 'eu-west-2_YfAipVOr3',
+  APP_CLIENT_ID: '7ivullticusoldoothf3f24kdb',
+  IDENTITY_POOL_ID: 'eu-west-2:44fc1650-f707-44ab-87b3-011d37529384'
 }
